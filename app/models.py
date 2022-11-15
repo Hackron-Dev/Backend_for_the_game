aa7951e4efcc0fd354daf81c95ce02e713d1cf9b
Hackron-Dev <ivanmyasnikov004@gmail.com>
2022-11-15 22:43:23 +0300

    Added TODO

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Table, Column, String, Integer
 
 from app.db.database import metadata
-
+# TODO 1:  Изменить так как ты хочешь
+# TODO 2:  переименовать score в Mcoin и добавить Rcoin
 users = Table('users', metadata,
               Column('id', Integer, primary_key=True),
               Column('Login', String, nullable=False),
