10fb34cb331f5e1c0aa839999a653ceea79c85c0
azamtoiri <azamtoiri@gmail.com>
2022-12-17 23:20:06 +0300

    Shop fixed | Added

--- app/routers/shop.py
+++ app/routers/shop.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException, Depends, status
 
 from app import oauth2, schemas
 from app.oauth2 import get_current_user
-from app.models import Shop, Shop_Pydantic, ShopIn_Pydantic
+from app.models import Shop, Shop_Pydantic, ShopIn_Pydantic, User_Pydantic
 
 router = APIRouter(
     tags=["Shop"],
@@ -11,13 +11,11 @@ router = APIRouter(
 )
 
 
-# Get all items from shop
 @router.get("")
 async def get_items():
     return await Shop_Pydantic.from_queryset(Shop.all())
 
 
-# Get item by id
 @router.get("/{product_id}")
 async def get_item_by_id(product_id: int, user_id: int = Depends(get_current_user)):
     try:
@@ -27,14 +25,13 @@ async def get_item_by_id(product_id: int, user_id: int = Depends(get_current_use
     return await shop_obj
 
 
-@router.post("", status_code=status.HTTP_201_CREATED, response_model=Shop_Pydantic)  # Not copy-paste Create shop
-async def create_product(product: ShopIn_Pydantic, user_id: int = Depends(get_current_user)):
-    product_obj = await Shop.create(user=user_id,
-                                    **product.dict())  # чтобы добавить брать входные данные и добавлять в бд
-    return await Shop_Pydantic.from_tortoise_orm(product_obj)  # вернуть введенные данные
+@router.post("", status_code=status.HTTP_201_CREATED, response_model=Shop_Pydantic)
+async def create_product(product: ShopIn_Pydantic, user: User_Pydantic = Depends(get_current_user)):
+    product_obj = await Shop.create(**product.dict(), user_id=user.id)
+    return await Shop_Pydantic.from_tortoise_orm(product_obj)
 
 
-@router.delete("/{product_id}", status_code=status.HTTP_410_GONE, response_model=schemas.Status)  # Delete product
+@router.delete("/{product_id}", status_code=status.HTTP_410_GONE, response_model=schemas.Status)
 async def delete_product(product_id: int):
     deleted_count = await Shop.filter(id=product_id).delete()
     if not deleted_count:
